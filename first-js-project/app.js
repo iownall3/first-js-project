e58e0b6f586ee60ac3a6f5d313a93f92288798ff
ryan phan <ryanphan@ryans-MacBook-Pro.local>
2022-08-29 20:39:53 -0400

    added a reset button and text content

---
+++ first-js-project/app.js
@@ -0,0 +1,49 @@
+const answer = ["rock", "paper", "scissors"]
+let playerOne = 0;
+let computerOne = 0;
+
+
+
+function getComputerChoice(){
+    return answer[Math.floor(Math.random()*answer.length)];
+};
+
+function playRound(playerSelection, computerSelection){
+    computerSelection = getComputerChoice();
+    playerSelection = getComputerChoice();
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock'))
+        {
+        playerOne++;
+        return "Player 1 wins";}
+
+    else if ((computerSelection == 'rock' && playerSelection == 'scissors') ||
+            (computerSelection == 'scissors' && playerSelection == 'paper') ||
+            (computerSelection == 'paper' && playerSelection == 'rock'))
+        {;
+        computerOne++;
+        return "Computer Wins";}
+    else ((computerSelection == 'rock' && playerSelection == 'rock') ||
+        (computerSelection == 'scissors' && playerSelection == 'scissors') ||
+        (computerSelection == 'paper' && playerSelection == 'paper'))
+        {return "It's a tie";}
+}
+
+function game(){
+    for (let i = 0; i < 99; i++){
+    let playerChoice = prompt("Rock, Paper, scissors?").toLowerCase;
+    computerSelection = getComputerChoice();
+    let result = playRound(playerChoice, computerSelection);
+    console.log(`Your points ${playerOne}`);
+    console.log(`Computer points ${computerOne}`);
+    if (computerOne === 5){
+        console.log("Computer Wins")
+        break;
+    } else (playerOne === 5){
+        console.log("You win!")
+        break;
+    };
+}
+}
+    
