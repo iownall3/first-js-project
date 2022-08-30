6c6567899fd91a567cccba035beb996d6bda467a
ryan phan <ryanphan@ryans-MacBook-Pro.local>
2022-08-29 20:35:36 -0400

    created a reset button and added text content

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const answer = ["rock", "paper", "scissors"]
+const btn = document.querySelectorAll('button');
+const scoreboard = document.querySelector('.scoreboard');
+const winner = document.querySelector('.winner');
 let playerOne = 0;
 let computerOne = 0;
-
-
 
 function getComputerChoice(){
     return answer[Math.floor(Math.random()*answer.length)];
@@ -10,40 +11,87 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     computerSelection = getComputerChoice();
-    playerSelection = getComputerChoice();
+    //playerSelection = getComputerChoice();
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'rock'))
         {
         playerOne++;
-        return "Player 1 wins";}
-
-    else if ((computerSelection == 'rock' && playerSelection == 'scissors') ||
+        winner.textContent= `Player One Wins!`
+        } else if ((computerSelection == 'rock' && playerSelection == 'scissors') ||
             (computerSelection == 'scissors' && playerSelection == 'paper') ||
             (computerSelection == 'paper' && playerSelection == 'rock'))
-        {;
+        {
         computerOne++;
-        return "Computer Wins";}
-    else ((computerSelection == 'rock' && playerSelection == 'rock') ||
+        winner.textContent= `Computer One Wins!`
+        } else if ((computerSelection == 'rock' && playerSelection == 'rock') ||
         (computerSelection == 'scissors' && playerSelection == 'scissors') ||
         (computerSelection == 'paper' && playerSelection == 'paper'))
-        {return "It's a tie";}
+        {
+        winner.textContent= `It's a tie!`
+        } else if (playerSelection == 'reset'){
+        return;
+        };
+
+    if (computerOne === 5){
+            winner.textContent= `Computer One Wins the whole thing!`
+    }else if (playerOne === 5){
+            winner.textContent= `Player One Wins the whole thing!`
+        }
 }
 
-function game(){
-    for (let i = 0; i < 99; i++){
-    let playerChoice = prompt("Rock, Paper, scissors?").toLowerCase;
-    computerSelection = getComputerChoice();
-    let result = playRound(playerChoice, computerSelection);
-    console.log(`Your points ${playerOne}`);
-    console.log(`Computer points ${computerOne}`);
-    if (computerOne === 5){
-        console.log("Computer Wins")
-        break;
-    } else (playerOne === 5){
-        console.log("You win!")
-        break;
-    };
-}
-}
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('rock')){
+            playerSelection = 'rock';
+        } else if (button.classList.contains('scissors')){
+            playerSelection = 'scissors';
+        } else if (button.classList.contains('paper')){
+            playerSelection = 'paper';
+        } else if(button.classList.contains('reset')){
+            playerOne = 0;
+            computerOne = 0;
+            playerSelection = 'reset';
+        }
+       
+        playRound(playerSelection)
+
+        scoreboard.textContent = `Score Board: Player Score: ${playerOne} 
+        Computer Score: ${computerOne}`;
+
+        if(playerOne === 5){
+           winner.textContent= `Player One Wins! Reset to win again.`
+        } else if(computerOne === 5) {
+            winner.textContent = `Computer Wins! Reset to try again.`
+        }
+        
+        disableButton();
+        
+        resetGame();    
+    })});
+
+function disableButton(){
+    btn.forEach((button) => {
+        if (playerOne === 5 || computerOne === 5){
+            button.disabled = true;
+            } else button.disabled = false;
+
+        if (button.classList.contains('reset')){
+            button.disabled = false;
+        }
+    });
+};
+
+function resetGame(){
+    btn.forEach((button) => {
+        button.addEventListener('click', () => {
+        if (button.classList.contains('reset')){
+            playerOne == 0 && computerOne == 0;
+            winner.textContent = ``
+    }
+})})};
+
+
+
+
     
